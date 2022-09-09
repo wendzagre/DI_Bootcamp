a7537b02707412b0c98bff5cf6ccb245029e4b89
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-09 01:03:08 +0000

    DevineReponse2

---
+++ Semaine2/Jour1/ExercicesXP/Exercice5/script.js
@@ -0,0 +1,63 @@
+typeof(15)
+//Prediction : nombre
+//Actual : number
+
+typeof(5.5)
+//Prediction : nombre
+//Actual : number
+
+typeof(NaN)
+// Prediction: nombre
+// Actual: number
+
+typeof("hello")
+// Prediction: chaine de caractère
+// Actual: string
+
+typeof(true)
+// Prediction:booléen
+// Actual:boolean
+
+typeof(1 != 2)
+// Prediction:booléen
+// Actual:boolean
+
+"hamburger" + "s"
+// Prediction: concaténation
+// Actual: hamburgers
+
+"hamburgers" - "s"
+// Prediction: valeur utilisée pour representer une quantité qui n'est pas un nombre
+// Actual:NaN
+
+"1" + "3"
+// Prediction: concaténation
+// Actual:13
+
+"1" - "3"
+// Prediction:soustraction
+// Actual: -2
+
+"johnny" + 5
+// Prediction:concaténation
+// Actual:johnny5
+
+"johnny" - 5
+// Prediction:valeur utilisée pour representer une quantité qui n'est pas un nombre
+// Actual:NaN
+
+99* "hello"
+// Prediction:valeur utilisée pour representer une quantité qui n'est pas un nombre
+// Actual:NaN
+
+1 != 1
+// Prediction:booléen
+// Actual:false
+
+1 == "1"
+// Prediction:booléen
+// Actual:true
+
+1 === "1"
+// Prediction:booléen
+// Actual:false
