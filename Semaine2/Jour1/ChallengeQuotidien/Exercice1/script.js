bbee0d9d660cff732f517ca1b0416dec8415bcf8
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-09 21:52:05 +0000

    challengeExercice1

---
+++ Semaine2/Jour1/ChallengeQuotidien/Exercice1/script.js
@@ -0,0 +1,15 @@
+let  fruits=["banana","Apples","Oranges","Bluberries"];
+fruits.splice(0,1); // supprime banana du tableau
+console.log(fruits); // affiche le tabbleau
+
+fruits.sort(); // trie le tableau par ordre alphabétique
+console.log(fruits); // affiche le tableau trié par ordre alphabétique
+
+fruits.push("kiwi"); // ajoute un élément à la fin du tableau
+console.log(fruits); // affiche la fin de l'élément du tableau 
+
+fruits.shift(); // supprime le premier élément du tableau
+console.log(fruits); // affiche le premier élément du tableau
+
+fruits.reverse(); //inverse l'ordre des éléments dans un tableau
+console.log(fruits); // affiche l'inverse des éléments dans un tableau
