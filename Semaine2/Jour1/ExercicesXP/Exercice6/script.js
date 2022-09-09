f6994724314fe39a98670caec8ad56fb669d2fa6
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-09 17:03:37 +0000

    DevineReponse3

---
+++ Semaine2/Jour1/ExercicesXP/Exercice6/script.js
@@ -0,0 +1,55 @@
+5 + "34"
+// Prediction: concaténation 
+// Actual: 534
+
+5 - "4"
+// Prediction:soustraction
+// Actual:1
+
+10 % 5
+// Prediction:reste de la division entière 
+// Actual: 0
+
+5 % 10
+// Prediction: reste de la division entière entre deux nombres
+// Actual:5
+
+"Java" + "Script"
+// Prediction: concaténation des deux chaines java et script
+// Actual:javaScript
+
+" " + " "
+// Prediction: concaténation des deux guillemets
+// Actual:' '
+
+" " + 0
+// Prediction: concaténation des guillemets et 0
+// Actual: ' 0'
+
+true + true
+// Prediction:la somme des deux booléens
+// Actual:2
+
+true + false
+// Prediction: somme logique entre deux variables logiques
+// Actual:1
+
+false + true
+// Prediction: somme logique entre deux variables logiques
+// Actual: 1
+
+false - true  
+// Prediction: soustraction logique entre deux variables logiques
+// Actual: -1
+
+!true
+// Prediction:différent de true donne false.
+// Actual: false
+
+3 - 4
+// Prediction:la soustraction des deux nombres 3 et 4
+// Actual: -1
+
+"Bob" - "bill"
+// Prediction: Valeur utilisée pour representer une quantité qui n'est pas nombre
+// Actual:NaN
