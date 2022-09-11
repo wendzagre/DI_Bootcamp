35824f296577590801756ad4b14c2d3ed4557271
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-11 23:26:40 +0000

    Exercice5

---
+++ Semaine4/Jour1/ExercicesXP/Exercice5.py
@@ -0,0 +1,8 @@
+from tkinter.messagebox import showerror
+
+
+name="ZAGRE"
+age=23 
+shoe_size=42
+info="je suis "+name +",j'ai "+str(age)+ "ans" +",j'aime etre elegant et j'ai une pointure de "+str(shoe_size)+",j'apprends et jaime beaucoup lire."
+print(info)
