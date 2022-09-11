efc45f7fa72065be04ee7e9deff1674de9358efa
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-11 22:20:11 +0000

    Exercice3

---
+++ Semaine4/Jour1/ExercicesXP/Exercice3.py
@@ -0,0 +1,11 @@
+a=5<3
+print(a) #cela donne false parce que 5 est supérieur à 3
+
+b=3==3
+print(b) #cela donne true car 3 est égale à 3
+
+c=3=="3"
+print(c) #cela donne false car le nombre 3 n'est pas égale à la chaine de caractère 3
+
+d="hello"=="hello"
+print(d)  #cela donne true car la chaine de caractère hello est égale à la chaine de caractère hello
