ff11dc50a41ebff1cb50b4bcb2d2c493460aed7b
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-12 11:10:04 +0000

    Exercice2

---
+++ Semaine2/Jour2/ExercicesXP/Exercice2/script.js
@@ -0,0 +1,10 @@
+let newDog="Chihuahua";
+console.log(newDog.length);
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+if (newDog=="Chihuahua"){
+    console.log("j'adore les Chihuahuas")
+}
+else{
+    console.log("je m'en fous , je préfère les chats")
+}
