0268f593d1466498e8fa5475cbd782a45fa581f3
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-15 12:36:31 +0000

    Exercice3

---
+++ Semaine4/Jour2/ExercicesXP/Exercice3.py
@@ -0,0 +1,14 @@
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana") #Supprimer banana de la liste
+print(basket)
+basket.remove("Blueberries") #Supprimer blueberries de la liste
+print(basket)
+basket.append("Kiwi") #Ajouter kiwi de la liste
+print(basket)
+basket.insert(0,"Pommes") #Ajouter une pomme au debut de la liste
+print(basket)
+print(basket.count("Pommes"))
+basket.clear()
+print(basket)
+
+
