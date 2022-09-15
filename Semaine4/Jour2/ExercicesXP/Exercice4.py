170407305096a128e4180972d569b2df814d9f60
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-15 23:53:48 +0000

    Exercice4

---
+++ Semaine4/Jour2/ExercicesXP/Exercice4.py
@@ -0,0 +1,10 @@
+""" 1- Un float sont les nombres rationnels qui se terminent généralement par un chiffre décimal
+La différence entre un entier et un float est qu'un entier répresente des données numériques qui represente
+des nombres entiers de moins l'infini à l'infini. Un float represente les nombres décimal plus précisement les nombres à virgules.
+"""
+#2
+nombreFlottant=[4.5,8.6,3.2,1.5]
+print(nombreFlottant)
+
+#3
+nombre=[1.5,2,2.5,3,3.5,4,4.5,5]
