6b873e269875654d050d142c8991db72f0324041
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-15 12:48:25 +0000

    Exercice3

--- Semaine4/Jour2/ExercicesXP/Exercice3.py
+++ Semaine4/Jour2/ExercicesXP/Exercice3.py
@@ -7,8 +7,9 @@ basket.append("Kiwi") #Ajouter kiwi de la liste
 print(basket)
 basket.insert(0,"Pommes") #Ajouter une pomme au debut de la liste
 print(basket)
-print(basket.count("Pommes"))
-basket.clear()
+print(basket.count("Pommes")) #Compter combien de pommes se trouvent dans la liste
+basket.clear() #vider le pannier
 print(basket)
+
 
 
