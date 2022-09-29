1f69667ced6b07218c33c480f06fdf54012ba98f
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-09-29 18:30:48 +0000

    Exercice2

---
+++ Semaine2/jour3/ExercicesXP/Exercice2/script.js
@@ -0,0 +1,13 @@
+let colors = ['rouge', 'vert', 'bleu',"jaune","gris"];
+for (let i = 0; i < colors.length; i++) {
+  console.log("Mon choix n° "+i + " est "+colors[i]);
+}
+console.log("--------------------")
+
+let tableaucolors=['rouge','vert','bleu','jaune','gris']
+console.log("Mon 1er  choix est :"+tableaucolors[0])
+console.log("Mon 2ème choix est :"+tableaucolors[1])
+console.log("Mon 3ème choix est :"+tableaucolors[2])
+console.log("Mon 4ème choix est :"+tableaucolors[3])
+console.log("Mon 5ème choix est :"+tableaucolors[4])
+
