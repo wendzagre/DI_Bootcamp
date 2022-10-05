6d9100a3e3f25d46f2c8a3ac4a061e9927198593
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-10-05 01:04:26 +0000

    Jour4

--- Semaine2/Jour4/ExercicesXP/Exercice2/script.js
+++ Semaine2/Jour4/ExercicesXP/Exercice2/script.js
@@ -1,22 +1,17 @@
 function calculateTip(){
-    let facture=prompt("Quel est le montant de la facture : ")
-    if(facture>32500){
-    pourboire=facture*0.2;
-    montant=facture+pourboire;
-    alert("montant du pourboire et facture est:"+montant);
+    var facture=Number(prompt("Quel est le montant de la facture : ")) //saisir le montant de la facture.
+    if(facture<50){   // si le montant de la facture est inférieur à 50 laissez un pourboire de 20%.
+         var pourboire=facture*0.2;
     }
-}
-calculateTip()
-    /*
-    else if(facture==32500 && facture==130000){
-      pourboire= facture*0.15;
-      montant=facture+pourboire;
-      alert("montant du pourboire et facture est:"+montant);
-    }
-    else if(facture>130000){
-       pourboire=facture*0.1;
-       montant=facture+pourboire;
-       alert("montant du pourboire et facture est:"+montant);
-    }
-}*/
 
+    else if((facture==50) && (facture==200)){ // sinon si le montant de la facture est comprise entre 50 et 200 laissez un pourboire de 15%.
+        var pourboire=facture*0.15;
+    }
+
+    else{                //Sinon laissez un pourboire de 10%.
+        var pourboire=facture*0.1;
+    }
+
+    console.log("Le montant du pourboire et de la facture sont: "+(facture+pourboire)) //Affiche du montant de la facture et du pourboire.
+  }
+  calculateTip() //Appel de la fonction.
