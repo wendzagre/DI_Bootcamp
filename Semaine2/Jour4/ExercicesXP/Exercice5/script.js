8cb518f67c1f4fce7718ace33902818d4dfc5295
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-10-06 00:36:25 +0000

    jour4

--- Semaine2/Jour4/ExercicesXP/Exercice5/script.js
+++ Semaine2/Jour4/ExercicesXP/Exercice5/script.js
@@ -1,15 +1,15 @@
 function changeEnough(itemPrice, amountOfChange){
-        let quarters=amountOfChange[0]*0.25 ;
-        let dimes=amountOfChange[1]*0.1  ;
-        let nickel=amountOfChange[2]*0.05;
-        let penny=amountOfChange[3]*0.01 ;
-        let sommeChangement=quarters+dimes+nickel+penny ;
-        if(sommeChangement>=itemPrice){
+        let quarters=amountOfChange[0]*0.25 ; //la valeur 1 de l'index 0 multiplier par 0.25.
+        let dimes=amountOfChange[1]*0.1  ; //la valeur 2 de l'index 1 multiplier par 0.1.
+        let nickel=amountOfChange[2]*0.05; //la valeur 3 de l'index 2 multiplier par 0.05.
+        let penny=amountOfChange[3]*0.01 ; //la valeur 4 de l'index 3 multiplier par 0.01.
+        let sommeChangement=quarters+dimes+nickel+penny ; //Fait la somme totale des différentes valeurs.
+        if(sommeChangement>=itemPrice){  // si la somme totale est supérieure au prix de l'article return true.
             console.log("true")
         }
     
         else{
-            console.log("false")
+            console.log("false") // sinon return false.
         }
     }
 changeEnough(4.25, [25, 20, 5, 0])
