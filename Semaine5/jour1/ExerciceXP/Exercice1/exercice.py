9f3cc6b34807d5c99d8f75f38d604f1f7dad65e1
ZAGRE Patrick <patrickzagre3@gmail.com>
2023-01-31 06:28:10 +0530

    changement

--- Semaine5/jour1/ExerciceXP/Exercice1/exercice.py
+++ Semaine5/jour1/ExerciceXP/Exercice1/exercice.py
@@ -5,18 +5,18 @@ class Cat:
         self.age = cat_age
 
 #Instanciation de 03 cats à l'aide du code fourni ci-dessus
-chat1 =Cat("miauh",6)
-chat2 =Cat("brewh",4)
-chat3 =Cat("ratis",2)
+bouli=Cat("milou",45)
+bouki=Cat("toupass",32)
+milou=Cat("bobi",25)
 #En dehors de la classe, trouvons une fonction quit trouve le chat le plus ancien et rencoie le chat
-def ancien_chat(chat1,chat2,chat3):
-	if(chat1.age>chat2.age and cha1.age>chat3.age):
-		print(f"le nom du chat est==> {cha1.name} , et il a {chat1.age}  ans. ")
-	elif(chat2.age>cha1.age and chat2.age>chat3.age):
-		print(f"le nom du chat est==>  {chat2.name} ,et il a {chat2.age}  ans. ")
-	elif(chat3.age>chat2.age and chat3.age>cat1.age):
-		print(f"le nom du chat est==>  {chat3.name} , et il a {chat3.age}  ans. ")
-	else:
-		print("Aucun chat")
+def ChatAncien(bouli,bouki,milou):
+    if(bouli.age>bouki.age and bouli.age>milou.age):
+        print('Le chat le plus âge est ',bouli.name, 'et a',bouli.age,'ans')
+    elif(bouki.age>bouli.age and bouki.age>milou.age):
+        print('Le chat le plus âge est ',bouki.name,'et a',bouki.age,'ans')
+    elif(milou.age>bouki.age and milou.age>bouli.age):
+        print('Le chat le plus âge est ',milou.name,'et a',milou.age,'ans')
+    else:
+        print("pas de résultat")
 #Impression de la chaine du chien le plus agé
-ancien_chat(chat1,chat2,cha3)
+ChatAncien(bouli, bouki, milou)
