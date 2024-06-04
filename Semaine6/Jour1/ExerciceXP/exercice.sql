98aac0e1c8c1370d07ee235cbc198faa8845cd0c
ZAGRE PATRICK Wendkuuni <patrickzagre3@gmail.com>
2024-06-04 11:34:00 +0000

    Texte Modifié et Commenté

--- Semaine6/Jour1/ExerciceXP/exercice.sql
+++ Semaine6/Jour1/ExerciceXP/exercice.sql
@@ -1,40 +1,58 @@
-#Ajout de deux tables items et 
-CREATE TABLE items (	
-		id_items SERIAL PRIMARY KEY,
-		elements VARCHAR (50) NOT NULL,
-		prix INTEGER NOT NULL
-	);
+-- Créez une base de données appelée public.
+CREATE DATABASE public ; 
 
-CREATE TABLE customers(
-	id SERIAL PRIMARY KEY,
- 	first_name VARCHAR (50) NOT NULL,
- 	last_name VARCHAR (60) NOT NULL
+-- Ajoutez deux tableaux 
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY , 
+  nom Varchar (100) NOT NULL , 
+  prix DECIMAL (10,2) NOT NULL
 );
-#Ajout des éléments dans la table des éléments
-INSERT INTO items(elements,prix)
-VALUES
-	('Petit bureau',100),
-	('Grand bureau',300),
-	('Ventilateur',80);
-#Ajout de 05 nouveaux clients dans la table clients	
-INSERT INTO customers(first_name,last_name)
-VALUES
-	('Greg','Jones'),
-	('Sandra','Jones'),
-	('Scott','Scott'),
-	('Trevor','Vert'),
-	('Mélanie','Johnson');
 
-#Récupération des données de la base de données
-#Tous les articles
-SELECT * FROM items;
-#Tous les articles dont le prix est supérieur à 80 (80 non inclus).
-SELECT * FROM items WHERE price >80;
-#Tous les articles dont le prix est inférieur à 300. (300 inclus)
-SELECT * FROM items WHERE price <=300;
-#Tous les clients dont le nom de famille est « Smith » (Quel sera votre résultat ?).
-SELECT * FROM customers WHERE last_name='Smith'; #Aucune sortie
-#Tous les clients dont le nom de famille est 'Jones'.
-SELECT * FROM customers WHERE last_name = 'Jones'
-#Tous les clients dont le prénom n'est pas 'Scott'.
-SELECT * FROM customers WHERE first_name !='Scott'; 
+
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY , 
+  nom Varchar (100) NOT NULL , 
+  prenom Varchar (150) NOT NULL
+);
+
+-- Ajoutez les éléments suivants au tableau des éléments 
+INSERT INTO items (nom,prix) VALUES ('Petit bureau',100) ; 
+INSERT INTO items (nom,prix) VALUES ('Grand bureau',300) ; 
+INSERT INTO items (nom,prix) VALUES ('Ventilateur',80) ; 
+
+-- Ajoutez 5 nouveaux clients à la table des clients 
+INSERT INTO customers (nom,prenom) VALUES ('Jones','Greg') ; 
+INSERT INTO customers (nom,prenom) VALUES ('Jones','Sandra') ; 
+INSERT INTO customers (nom,prenom) VALUES ('Scott','Scott') ; 
+INSERT INTO customers (nom,prenom) VALUES ('Vert', 'Trevor') ; 
+INSERT INTO customers (nom,prenom) VALUES ('Johnson','Mélanie') ; 
+
+-- ou 
+INSERT INTO customers (nom,prenom) VALUES 
+('Jones','Greg'),
+('Jones','Sandra'),
+('Scott','Scott'),
+('Vert', 'Trevor'),
+('Johnson','Mélanie'); 
+
+-- Utilisez SQL pour récupérer les données suivantes de la base de données
+-- 1- Tous les articles
+SELECT * FROM items ; 
+
+-- 2- Tous les articles dont le prix est supérieur à 80 (80 non inclus)
+SELECT * FROM items WHERE prix > 80 ;
+
+-- 3- Tous les articles dont le prix est inférieur à 300. (300 inclus)
+SELECT * FROM items WHERE prix <= 300 ; 
+
+-- 4-Tous les clients dont le nom de famille est « Smith 
+SELECT * FROM customers WHERE nom = 'Smith' ; 
+
+-- 5- Tous les clients dont le nom de famille est « Jones ».
+SELECT * FROM customers WHERE nom = 'Jones' ;
+
+-- 6- Tous les clients dont le prénom n'est pas « Scott »
+SELECT * FROM customers WHERE prenom != 'Scott' ; 
+   -- ou 
+SELECT * FROM customers WHERE prenom <> 'Scott' ; 
