bf235d63851313f4823cfeca7e3f2dd6479f0036
ZAGRE PATRICK Wendkuuni <patrickzagre3@gmail.com>
2024-06-06 14:35:22 +0000

    précision

--- Semaine6/Jour1/DefiQuotidien/exercice.sql
+++ Semaine6/Jour1/DefiQuotidien/exercice.sql
@@ -6,4 +6,4 @@ INSERT INTO actors (first_name, last_name, age, number_oscars)
 VALUES(NULL,NULL,NULL,NULL);
 
 -- Selon vous, quel sera le résultat ?
--- Résultat : Erreur, les champs ne peuvent pas être NULL.
+-- Résultat : Erreur, les champs ne peuvent pas être NULL à cause de la contrainte NOT NULL.
